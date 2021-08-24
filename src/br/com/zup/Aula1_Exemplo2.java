e922c39fe24f4123410250f2bb7fde9923696398
Debora Souza <deboral.p@hotmail.com>
2021-08-24 14:45:41 -0300

    Adicionando operação de subtração

--- src/br/com/zup/Aula1_Exemplo2.java
+++ src/br/com/zup/Aula1_Exemplo2.java
@@ -4,12 +4,17 @@ public class Aula1_Exemplo2 {
     public static void main(String[] args) {
 
         //Armazenar os valores da memória
-       int numero1 = 20;
+       int numero1 = 100;
        int numero2 = 30;
        //Criando a operação de soma
        int resultadoDaSoma = numero1 + numero2;
+       //Criando a operação de subtração
+        int resultadoDaSubtração = numero1 - numero2;
 
        // Exibindo a operação de soma para o usuário
-       System.out.print("O resultado é "+ resultadoDaSoma );
+       System.out.println("O resultado da soma é "+ resultadoDaSoma );
+
+        //Exibindo a operação de subtração para o usuario
+        System.out.println("O resultado ´da subtração é "+ resultadoDaSubtração );
     }
 }
