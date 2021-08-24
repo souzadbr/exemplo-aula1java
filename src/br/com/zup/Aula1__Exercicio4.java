4aa3410f4c9a587748836115c10c76547e6ca13a
Debora Souza <deboral.p@hotmail.com>
2021-08-24 15:29:05 -0300

    Exercicio 4 adicionado e finalizado

---
+++ src/br/com/zup/Aula1__Exercicio4.java
@@ -0,0 +1,22 @@
+package br.com.zup;
+
+public class Aula1_Exercicio4 {
+    public static void main(String[] args) {
+        //Exercico 4 lista
+        //Armazenou os dois dados necessarios para o processamento
+        float qtDeHorasTrabalhadas = (float) 40;
+        float salarioPorhora = (float) 45.7;
+
+        //Calcular o salario bruto
+        float salarioBruto = qtDeHorasTrabalhadas * salarioPorhora;
+
+        System.out.println(" Seu salário Bruto é "+ "$" + salarioBruto);
+
+
+
+
+
+
+
+    }
+}
