cee67efd1e1f80bac63953a3255acbf66d573fd1
Debora Souza <deboral.p@hotmail.com>
2021-08-24 13:53:04 -0300

    Commit Inicial

---
+++ src/br/com/zup/Exemplo1.java
@@ -0,0 +1,21 @@
+package br.com.zup;
+
+public class Exemplo1 {
+    public static void main(String[] args) {
+
+        int idadeDoUsuario = 29;
+        String nomeDoUsuario = "Debora";
+
+        System.out.println(idadeDoUsuario);
+        System.out.println(nomeDoUsuario);
+
+        System.out.print("Olá, meu nome é ");
+        System.out.print(nomeDoUsuario);
+        System.out.println(" e minha idade é ");
+        System.out.print(idadeDoUsuario);
+        System.out.print (" anos");
+
+
+
+    }
+}
