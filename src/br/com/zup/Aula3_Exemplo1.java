483b714103b916f230253fed2fd01be0e0bfc709
Debora Souza <deboral.p@hotmail.com>
2021-08-26 10:38:03 -0300

    Aula3_Exemplo 1 adicionado

---
+++ src/br/com/zup/Aula3_Exemplo1.java
@@ -0,0 +1,21 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Aula3_Exemplo1 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Por favor digite o número1:" );
+        double numero1 = leitor.nextDouble();
+
+        System.out.println("Por favor digite o numero2:");
+        double numero2 = leitor.nextDouble();
+
+        if (numero1>numero2) {
+            System.out.println( "O número 1 é o maior");
+        } else  {
+            System.out.println("O número 2 é o maior");
+        }
+    }
+}
