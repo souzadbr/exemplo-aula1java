7683142f8c2ed66a46e3d515ba84b60a439331a4
Debora Souza <deboral.p@hotmail.com>
2021-08-31 17:04:38 -0300

    Exercicio Level UP finalziado

---
+++ src/br/com/zup/Aula6/Aula6_ExercicioLevelUp_ListaComplementar.java
@@ -0,0 +1,33 @@
+package br.com.zup.Aula6;
+
+import java.util.Scanner;
+
+public class Aula6_ExercicioLevelUp_ListaComplementar {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int contador = 0;
+        double numImpares = 0;
+        double soma = 0;
+
+        System.out.println("Digite o inicio do seu intervalo:");
+        double inicioIntervalo = leitor.nextDouble();
+        System.out.println("Digite o Fim do seu intervalor:");
+        double fimIntervalo = leitor.nextDouble();
+        if (inicioIntervalo > fimIntervalo) {
+            System.out.println("Intervalo de valores inválidos, digite novamente");
+        }
+        System.out.println("O internvalo é "+inicioIntervalo +" até "+fimIntervalo);
+
+        while (inicioIntervalo < fimIntervalo) {
+
+            if (inicioIntervalo % 2 != 0) {
+                System.out.println("Os números Impares são: " + inicioIntervalo);
+                soma = soma + inicioIntervalo;
+            }
+            inicioIntervalo++;
+
+        }
+        System.out.println(" a Soma é " + soma);
+    }
+}
