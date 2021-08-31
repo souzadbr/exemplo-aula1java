ade86f091472a507b42f995eab01f69a47cad303
Debora Souza <deboral.p@hotmail.com>
2021-08-31 16:23:48 -0300

    Exercicio 5 Aula 6 finalizado

---
+++ src/br/com/zup/Aula6/Aula6_Exercicio5_ListaComplementar.java
@@ -0,0 +1,13 @@
+package br.com.zup.Aula6;
+
+public class Aula6_Exercicio5_ListaComplementar {
+    public static void main(String[] args) {
+
+
+        int contador = 0;
+                while(contador <=100000){
+                    System.out.println(contador);
+                    contador = contador + 1000;
+                }
+    }
+}
