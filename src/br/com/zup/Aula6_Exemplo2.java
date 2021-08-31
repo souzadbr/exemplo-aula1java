19408f193cd498a295ea6600024aa38ee016ff04
Debora Souza <deboral.p@hotmail.com>
2021-08-31 13:35:52 -0300

    Adicionando exemplo2 na Aula6

---
+++ src/br/com/zup/Aula6_Exemplo2.java
@@ -0,0 +1,21 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aula6_Exemplo2 {
+    public static void main(String[] args) {
+        List<String> alunos = new ArrayList<>();
+       // List<Integer> notas = new ArrayList<>();
+
+        alunos.add("Débora");
+        alunos.add("Paulo");
+        alunos.add("Raquel");
+
+        System.out.println( alunos);
+
+        alunos.remove("Raquel");
+        alunos.add("Danilo");
+        System.out.println(alunos.get(2));
+    }
+}
