ea855d674f1b54044d2a4b6ce4fa4a30c066dd7f
Debora Souza <deboral.p@hotmail.com>
2021-09-01 16:04:52 -0300

    Exericio 2 lista Extra Aula6

---
+++ src/br/com/zup/Aula6/Aula6_ExercicioExtra2.java
@@ -0,0 +1,32 @@
+package br.com.zup.Aula6;
+
+import java.util.Scanner;
+
+public class Aula6_ExercicioExtra2 {
+    public static void main(String[] args) {
+        /*
+        2. Faça um programa que calcule e mostre a soma dos 50 primeiros números ímpares.
+
+         */
+
+        int inicioIntervalo = 1;
+        int finalIntervalo = 51;
+        int soma = 0;
+        int numeroImpares = 0;
+
+
+        System.out.println("Esse programa vai calcular os 50 primeiro números impares dentro do intervalo desejado");
+
+        while (inicioIntervalo <= finalIntervalo) {
+            if (inicioIntervalo % 2 != 0) {
+                numeroImpares = inicioIntervalo;
+                soma = soma + numeroImpares;
+                System.out.println(numeroImpares); //Mostrando os numeros para ter certeza que estão aparecendo apenas impares.
+            }
+
+            inicioIntervalo++;
+        }
+
+        System.out.println("A soma dos números Impares dos primeiros 50 números é " + soma);
+    }
+}
