02e7a6f4dc039e2f96e1b1eeb9868b2e0f3a3701
Debora Souza <deboral.p@hotmail.com>
2021-09-02 09:15:00 -0300

    Aula7 Exercicio 10 - usando while e if para media

---
+++ src/br/com/zup/Aula7/Aula7_Exercicio10.java
@@ -0,0 +1,35 @@
+package br.com.zup.Aula7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Aula7_Exercicio10 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        List<Double>notas = new ArrayList<>();
+
+        double quantidadeNotas =4;
+        int contador =1;
+        double notasInserida =0;
+        double soma = 0;
+        double média =0;
+
+        while(contador<=quantidadeNotas){
+            System.out.println("Digite a nota "+contador+":");
+            notasInserida= leitor.nextDouble();
+
+            if(notasInserida>=0){
+                notas.add(notasInserida);
+                System.out.println(notas);
+                contador++;
+               soma = soma + notasInserida;
+
+            }
+
+        }
+        System.out.println("A soma das 4 notas é "+soma);
+        System.out.println("A média das notas inseridas é "+soma/quantidadeNotas);
+    }
+}
